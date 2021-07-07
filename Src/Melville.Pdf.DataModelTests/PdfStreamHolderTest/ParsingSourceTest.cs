2092afd98f8e0b8385efae08f10e0514b7d7ba24
John Melville <JohnMelville@gmail.com>
2021-07-07 10:55:34 -0400

    ParsingSource can jump arround the stream.

---
+++ Src/Melville.Pdf.DataModelTests/PdfStreamHolderTest/ParsingSourceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+using Melville.Pdf.DataModelTests.ParsingTestUtils;
+using Melville.Pdf.LowLevel.Parsing.PdfStreamHolders;
+using Xunit;
+
+namespace Melville.Pdf.DataModelTests.PdfStreamHolderTest
+{
+    public class ParsingSourceTest
+    {
+        private static MemoryStream IndexedStream()
+        {
+            var ret = new byte[256];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = (byte)i;
+            }
+
+            return new MemoryStream(ret);
+        }
+
+        private readonly ParsingSource sut = new(IndexedStream());
+
+        private SequencePosition ConfirmBytes(ReadOnlySequence<byte> seq, params byte[] values)
+        {
+            var reader = new SequenceReader<byte>(seq);
+            foreach (var value in values)
+            {
+                AAssert.True(reader.TryRead(out var b));
+                Assert.Equal(value, b);
+            }
+
+            return reader.Position;
+        }
+        [Fact]
+        public async Task ReadFiveBytes()
+        {
+            var result = await sut.ReadAsync();
+            var sp =ConfirmBytes(result.Buffer, 0, 1, 2, 3, 4);
+            Assert.Equal(0, sut.Position);
+            sut.AdvanceTo(result.Buffer, sp);
+            Assert.Equal(5, sut.Position);
+        }
+        [Fact]
+        public async Task ReadThenJump()
+        {
+            var result = await sut.ReadAsync();
+            var sp =ConfirmBytes(result.Buffer, 0, 1, 2, 3, 4);
+            Assert.Equal(0, sut.Position);
+            sut.AdvanceTo(result.Buffer, sp);
+            Assert.Equal(5, sut.Position);
+            sut.Seek(45);
+            result = await sut.ReadAsync();
+            sp = ConfirmBytes(result.Buffer, 45, 46, 47, 48);
+            sut.AdvanceTo(result.Buffer, sp);
+            Assert.Equal(49, sut.Position);
+        }
+    }
+}
---
+++ Src/Melville.Pdf.LowLevel/Parsing/PdfStreamHolders/ParsingSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Melville.Pdf.LowLevel.Parsing.PdfStreamHolders
+{
+    public class ParsingSource
+    {
+        private readonly Stream source;
+        public long Position { get; private set; }
+        private PipeReader reader;
+
+        public ParsingSource(Stream source)
+        {
+            this.source = source;
+            CreatePipeReader();
+        }
+
+        [MemberNotNull(nameof(reader))]
+        private void CreatePipeReader()
+        {
+            Position = source.Position;
+            reader = PipeReader.Create(source, new StreamPipeReaderOptions(leaveOpen:true));
+        }
+
+        public ValueTask<ReadResult> ReadAsync(CancellationToken token = default)
+        {
+            return reader.ReadAsync(token);
+        }
+
+        public void AdvanceTo(in ReadOnlySequence<byte> sequence, SequencePosition consumed) =>
+            AdvanceTo(sequence, consumed, consumed);
+        public void AdvanceTo(
+            in ReadOnlySequence<byte> sequence, SequencePosition consumed, SequencePosition examined)
+        {
+            Position += sequence.GetOffset(consumed);
+            reader.AdvanceTo(consumed);
+        }
+
+        public void Seek(long newPosition)
+        {
+            reader.Complete();
+            source.Seek(newPosition, SeekOrigin.Begin);
+            Position = newPosition;
+            CreatePipeReader();
+        }
+    }
+}
