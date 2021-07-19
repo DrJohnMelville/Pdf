654d56a613698b53bb4fbe40f7ee0d717dfaf0a7
John Melville <JohnMelville@gmail.com>
2021-07-19 19:04:50 -0400

    LowLevelReader Shows a blank window

---
+++ Src/Melville.Pdf.LowLevelReader/CompsitionRoot/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Melville.IOC.IocContainers;
+using Melville.WpfAppFramework.StartupBases;
+
+namespace Melville.Wpf.LowLevelReader.CompsitionRoot
+{
+    public class Startup: StartupBase
+    {
+        [STAThread]
+        public static void Main(string[] arguments)
+        {
+            ApplicationRootImplementation.Run(new Startup());
+        }
+
+        protected override void RegisterWithIocContainer(IBindableIocService service)
+        {
+        }
+    }
+}
