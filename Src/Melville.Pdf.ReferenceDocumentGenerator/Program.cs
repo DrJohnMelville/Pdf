561b5eac9579664f6fa39b69f29191d32ae75f73
John Melville <JohnMelville@gmail.com>
2021-07-31 13:37:42 -0400

    ReferenceDocumentGenerator prints the banner.

---
+++ Src/Melville.Pdf.ReferenceDocumentGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Melville.Pdf.ReferenceDocumentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Melville.Pdf.ReferenceDocumentGenerator: Generate PDF documents exercising the PDF standard.");
+            Console.WriteLine("-H for help");
+        }
+    }
+}
