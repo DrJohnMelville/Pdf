c17a0cf9480731e9206e865be73f5e7355663ae1
John Melville <JohnMelville@gmail.com>
2021-08-04 04:21:58 -0400

    BitWriterWorks

---
+++ Src/Melville.Pdf.DataModelTests/Writer/Lzw/BitWriterTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipelines;
+using System.Reflection;
+using System.Threading.Tasks;
+using Melville.Pdf.LowLevel.Filters.LzwFilter;
+using Melville.Pdf.LowLevel.Model.Primitives;
+using Xunit;
+
+namespace Melville.Pdf.DataModelTests.Writer.Lzw
+{
+    public class BitWriterTest
+    {
+        
+        [Theory]
+        [InlineData("\x01", 4,"\x10")]
+        [InlineData("\x07", 4,"\x70")]
+        [InlineData("\x07\x07", 4,"\x77")]
+        [InlineData("\x07\x07\x07", 4,"\x77\x70")]
+        [InlineData("\x07\x07\x07\x07", 4,"\x77\x77")]
+        [InlineData("\x07\x07\x07\x07\x07", 4,"\x77\x77\x70")]
+        [InlineData("\x07", 5,"8")]
+        [InlineData("\x07\x07", 5,"\x39\xC0")]
+        [InlineData("\x07\x07\x07", 5,"\x39\xCE")]
+        [InlineData("\x07\x07\x07\x07", 5,"\x39\xCE\x70")]
+        [InlineData("\x07\x07\x07\x07\x07", 5,"\x39\xCE\x73\x80")]
+        public async Task BitStringTest(string decoded, int bitsize, string encoded)
+        {
+            var target = new MemoryStream();
+            var writer = new BitWriter(PipeWriter.Create(target));
+            foreach (var character in decoded)
+            {
+                await writer.WriteBits((int) character, bitsize);
+            }
+            await writer.FinishWrite();
+            Assert.Equal(encoded, target.ToArray().ExtendedAsciiString());
+            
+        }
+    }
+}
---
+++ Src/Melville.Pdf.LowLevel/Filters/LzwFilter/BitWriter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace Melville.Pdf.LowLevel.Filters.LzwFilter
+{
+    public class BitWriter
+    {
+        private readonly PipeWriter target;
+        private byte residue;
+        private byte spotsAvailable;
+        
+
+        public BitWriter(PipeWriter target)
+        {
+            this.target = target;
+            residue = 0;
+            spotsAvailable = 8;
+        }
+
+        public async ValueTask WriteBits(int data, int bits)
+        {
+            if (spotsAvailable == 0) await WriteCurrentByte();
+            int leftOverBits = bits - spotsAvailable;
+            if (leftOverBits <= 0)
+            {
+                WriteBottomBits(data, bits);
+                return;
+            }
+
+            WriteBottomBits(data >> leftOverBits, spotsAvailable);
+            await WriteBits(data, leftOverBits);
+        }
+
+        private void WriteBottomBits(int data, int bits)
+        {
+            residue |= (byte) ((data & Mask(bits)) << (spotsAvailable - bits));
+            spotsAvailable = (byte) (spotsAvailable - bits);
+        }
+
+        private byte Mask(int bits) => (byte) ((1 << bits) - 1);
+
+        public ValueTask<FlushResult> FinishWrite()
+        {
+            if (spotsAvailable > 7) return new ValueTask<FlushResult>(new FlushResult());
+            return WriteCurrentByte();
+        }
+
+        private ValueTask<FlushResult> WriteCurrentByte()
+        {
+            WriteByte(target.GetSpan(1));
+            target.Advance(1);
+            return target.FlushAsync();
+        }
+
+        private void WriteByte(Span<byte> span)
+        {
+            span[0] = residue;
+            residue = 0;
+            spotsAvailable = 8;
+        }
+    }
+}
