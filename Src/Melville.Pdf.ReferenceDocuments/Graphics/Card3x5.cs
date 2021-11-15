f0ff45ce9cb484aa63a4352f404be649e677129f
John Melville <JohnMelville@gmail.com>
2021-11-15 04:49:12 -0500

    LineWidths reference document works.

---
+++ Src/Melville.Pdf.ReferenceDocuments/Graphics/Card3x5.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+using Melville.Pdf.LowLevel.Model.Wrappers;
+using Melville.Pdf.LowLevel.Writers.ContentStreams;
+
+namespace Melville.Pdf.ReferenceDocuments.Graphics;
+
+public abstract class Card3x5: CreatePdfParser
+{
+    protected Card3x5(string prefix, string helpText) : base(prefix, helpText)
+    {
+    }
+
+    public override async ValueTask WritePdfAsync(Stream target)
+    {
+        var docCreator = new PdfDocumentCreator();
+        var page = docCreator.Pages.CreatePage();
+        page.AddBox(BoxName.CropBox, new PdfRect(0,0, 5*72, 3*72));
+        SetPageProperties(page);
+        await page.AddToContentStreamAsync(CreateContentStream);
+        await docCreator.CreateDocument().WriteToAsync(target);
+    }
+    private ValueTask CreateContentStream(ContentStreamWriter csw)
+    {
+        DoPainting(csw);
+        return DoPaintingAsync(csw);
+    }
+    
+    protected virtual void SetPageProperties(PageCreator page)
+    {
+    }
+
+    protected virtual void DoPainting(ContentStreamWriter csw)
+    {
+    }
+
+    protected virtual ValueTask DoPaintingAsync(ContentStreamWriter csw) => ValueTask.CompletedTask;
+
+}
