d5348742f415bfd71d3925925a3184de94c0de5e
John Melville <JohnMelville@gmail.com>
2021-11-17 03:12:17 -0500

    4 view layout in the comparingViewer

--- Src/Melville.Pdf.ComparingReader/Renderers/TabMultiRendererViewModel.cs
+++ Src/Melville.Pdf.ComparingReader/Renderers/TabMultiRendererViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Melville.Pdf.ComparingReader.Viewers.LowLevel;
 
 namespace Melville.Pdf.ComparingReader.Renderers;
 
 public class TabMultiRendererViewModel : MultiRenderer
 {
+    public IRenderer First => Renderers[0];
+    public IRenderer Second => Renderers[1];
+    public IRenderer Third => Renderers[2];
+    public IEnumerable<IRenderer> Remaining => Renderers.Skip(3);
     public TabMultiRendererViewModel(IList<IRenderer> renderers) : base((IReadOnlyList<IRenderer>)renderers)
     {
     }
