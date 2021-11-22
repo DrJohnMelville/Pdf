845ded6610257224695c955f9fa00a3045a9d9f4
John Melville <JohnMelville@gmail.com>
2021-11-22 04:42:16 -0500

    About to implement multireaders.

---
+++ Src/Melville.Pdf.ReferenceDocuments/Graphics/GraphicProperties/GraphicsStateDictionary.cs
@@ -0,0 +1,30 @@
+﻿using Melville.Pdf.LowLevel.Writers.ContentStreams;
+
+namespace Melville.Pdf.ReferenceDocuments.Graphics.GraphicProperties;
+
+public class GraphicsStateDictionary:Card3x5
+{
+    private PdfName dictionaryName = NameDirectory.Get("GS1");
+
+    public GraphicsStateDictionary() : base("Line with Style set from GraphicStateDictionary")
+    {
+        
+    }
+
+    protected override void SetPageProperties(PageCreator page)
+    {
+        page.AddResourceObject(ResourceTypeName.ExtGState, dictionaryName, new DictionaryBuilder()
+            .WithItem(KnownNames.LW, new PdfInteger(25)).AsDictionary());
+    }
+
+    protected override async ValueTask DoPaintingAsync(ContentStreamWriter csw)
+    {
+        csw.MoveTo(0,0);
+        csw.LineTo(300, 300);
+        csw.StrokePath();
+        await csw.LoadGraphicStateDictionary(dictionaryName);
+        csw.MoveTo(0,300);
+        csw.LineTo(300,0);
+        csw.StrokePath();
+    }
+}
