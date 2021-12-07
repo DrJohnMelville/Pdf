4af71847e926589c2b24b60cb2028bdba4b690c4
John Melville <JohnMelville@gmail.com>
2021-12-07 18:50:25 -0500

    Implement multidimensional lookup table in the ICC parser.

--- Src/Melville.Icc/Model/Tags/MultidimensionalLookupTable.cs
+++ Src/Melville.Icc/Model/Tags/MultidimensionalLookupTable.cs
@@ -7,18 +7,28 @@ namespace Melville.Icc.Model.Tags;
 public class MultidimensionalLookupTable: IColorTransform
 {
     public IReadOnlyList<int> DimensionLengths { get; }
-    public IReadOnlyList<float> Points { get; }
+
+    private readonly float[] points;
+    public IReadOnlyList<float> Points => points;
 
     public int Inputs { get; }
     public int Outputs { get; }
-    
+
+    public MultidimensionalLookupTable(
+        IReadOnlyList<int> dimensionLengths, int outputs, params float[] points)
+    {
+        DimensionLengths = dimensionLengths;
+        this.points = points;
+        Outputs = outputs;
+    }
+
     public MultidimensionalLookupTable(ref SequenceReader<byte> reader)
     {
         reader.Skip32BitPad();
         Inputs = reader.ReadBigEndianUint16();
         Outputs = reader.ReadBigEndianUint16();
         DimensionLengths = ParseDimensionList(ref reader);
-        Points = reader.ReadIEEE754FloatArray(TotalPoints());
+        points = reader.ReadIEEE754FloatArray(TotalPoints());
     }
     
     public MultidimensionalLookupTable(ref SequenceReader<byte> reader, int outputs)
@@ -30,7 +40,7 @@ public class MultidimensionalLookupTable: IColorTransform
         Outputs = outputs;
         Inputs = DimensionLengths.Count(i => i > 0);
         var tableEntries = TotalPoints();
-        Points = reader.ReadScaledFloatArray(tableEntries, dimensionSize);
+        points = reader.ReadScaledFloatArray(tableEntries, dimensionSize);
     }
 
     private int TotalPoints() => DimensionLengths.Aggregate(Outputs, (agg, i) => agg * Math.Max(1, i));
@@ -46,12 +56,48 @@ public class MultidimensionalLookupTable: IColorTransform
     }
     public void Transform(in ReadOnlySpan<float> input, in Span<float> output)
     {
-        throw new NotImplementedException();
+        Span<int> indices = stackalloc int[input.Length];
+        Span<float> scaledInputs = stackalloc float[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            scaledInputs[i] = Math.Clamp(input[i], 0, 1) * (DimensionLengths[i]-1);
+        }
+        InnerTransform(scaledInputs, indices, indices.Length -1, output);
     }
 
     private void InnerTransform(
-        in ReadOnlySpan<float> inputs, in Span<int> indices, int activeIndex, in Span<float> output)
+        in Span<float> inputs, in Span<int> indices, int activeIndex, in Span<float> output)
     {
-        
+        if (activeIndex < 0)
+        {
+            Lookup(indices, output);
+            return;
+        }
+        indices[activeIndex] = (int)inputs[activeIndex];
+        var fraction = inputs[activeIndex] - indices[activeIndex];
+        InnerTransform(inputs, indices, activeIndex-1, output);
+        indices[activeIndex]++;
+        if (IsMaximumSpecialCase(indices, activeIndex)) return;
+        Span<float> highResult = stackalloc float[output.Length];
+        InnerTransform(inputs, indices, activeIndex-1, highResult);
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = Interpolation.InterpolateFraction(fraction, output[i], highResult[i]);
+        }
+    }
+    private bool IsMaximumSpecialCase(Span<int> indices, int activeIndex) =>
+        indices[activeIndex] >= DimensionLengths[activeIndex];
+
+    public void Lookup(in Span<int> indices, Span<float> output)
+    {
+        var size = Outputs;
+        int position = 0;
+        for (int i = indices.Length -1; i >= 0; i--)
+        {
+            position += size * indices[i];
+            size *= DimensionLengths[i];
+        }
+        points.AsSpan().Slice(position, Outputs).CopyTo(output);
     }
 }
---
+++ Src/Melville.Pdf.DataModelTests/ICC/MultiProcessClutTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Melville.Icc.Model.Tags;
+using Xunit;
+
+namespace Melville.Pdf.DataModelTests.ICC;
+
+public class MultiProcessClutTest
+{
+    [Theory]
+    [InlineData(0,0,  0,0)]
+    [InlineData(1,1,  5,6)]
+    [InlineData(0.5, 0.5,   2.5, 3)]
+    [InlineData(0.5, 0,   1, 2.5)]
+    public void SimpleClut(float in0, float in1, float out0, float out1)
+    {
+        var sut = new MultidimensionalLookupTable(new int[]{2,2}, 2, 
+            0, 0, 3, 1, 2, 5, 5, 6);
+
+        Verify(sut, in0, in1, out0, out1);
+    }
+
+    private void Verify(MultidimensionalLookupTable sut, float in0, float in1, float out0, float out1)
+    {
+        Span<float> input = stackalloc float[] { in0, in1 };
+        Span<float> output = stackalloc float[2];
+        sut.Transform(input, output);
+        Assert.Equal(out0, output[0]);
+        Assert.Equal(out1, output[1]);
+        
+    }
+}
