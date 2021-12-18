a2a3a2b38607657e04fe569e5faa13ebfc931f32
John Melville <JohnMelville@gmail.com>
2021-12-18 09:52:35 -0500

    Doesn't compile -- transfer to laptop.

--- Src/Melville.Pdf.Model/Renderers/Bitmaps/IPdfBitmap.cs
+++ Src/Melville.Pdf.Model/Renderers/Bitmaps/IPdfBitmap.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Buffers;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.IO.Pipelines;
 using System.Threading.Tasks;
+using Melville.Icc.Model.Tags;
 using Melville.Pdf.LowLevel.Model.Conventions;
 using Melville.Pdf.LowLevel.Model.Objects;
 
@@ -9,6 +14,7 @@ public interface IPdfBitmap
 {
     int Width { get; }
     int Height { get; }
+
     /// <summary>
     /// Fill the bitmap pointed to by buffer.
     /// This is implemented as an unsafe pointer operation so that I can quickly fill native buffers,
@@ -21,34 +27,81 @@ public interface IPdfBitmap
 
 public static class PdfBitmapOperatons
 {
-    public static int ReqiredBufferSize(this IPdfBitmap bitmap) => 4 * bitmap.Width * bitmap.Height;
+    public static int ReqiredBufferSize(this IPdfBitmap bitmap) => 4 * TotalPixels(bitmap);
+    public static int TotalPixels(this IPdfBitmap bitmap) => bitmap.Width * bitmap.Height;
 
     public static async ValueTask<IPdfBitmap> WrapForRenderingAsync(this PdfStream stream) =>
-        new PdfBitmapWrapper(stream,
+        new PdfBitmapWrapper(PipeReader.Create(await stream.StreamContentAsync()),
             (int)await stream.GetOrDefaultAsync(KnownNames.Width, 1),
             (int)await stream.GetOrDefaultAsync(KnownNames.Height, 1)
         );
 }
 
-public class PdfBitmapWrapper: IPdfBitmap
+public class PdfBitmapWrapper : IPdfBitmap
 {
     public int Width { get; }
     public int Height { get; }
-    private readonly PdfStream source;
+    private readonly PipeReader source;
 
-    public PdfBitmapWrapper(PdfStream source, int width, int height)
+    public PdfBitmapWrapper(PipeReader source, int width, int height)
     {
         this.source = source;
         Width = width;
         Height = height;
     }
-    
+
     public unsafe ValueTask RenderPbgra(byte* buffer)
     {
-        for (int i = 0; i < this.ReqiredBufferSize(); i++)
+        var x = new BitmapWriter(buffer,source, this.TotalPixels());
+        return InnerRender(x);
+    }
+
+    private async ValueTask InnerRender(BitmapWriter c)
+    {
+        int row = Height - 1;
+        int column = 0;
+        while (c.LoadLPixels(await c.reader.ReadAsync(), ref row, ref column)) {/* do nothing*/}
+
+    }
+}
+
+unsafe readonly struct BitmapWriter
+{
+    private readonly byte* buffer;
+    public readonly PipeReader reader;
+    public readonly int totalPixels;
+    private const int BytesPerPixel = 4; 
+    
+    public BitmapWriter(byte* buffer, PipeReader reader, int totalPixels)
+    {
+        this.buffer = buffer;
+        this.reader = reader;
+        this.totalPixels = totalPixels;
+    }
+
+
+    public bool LoadLPixels(ReadResult readResult, ref int row, ref int col)
+    {
+        if (readResult.IsCompleted && readResult.Buffer.Length < BytesPerPixel) return false;
+        var localPointer = buffer + (currentPixel * 4);
+        
+        var seq = new SequenceReader<byte>(readResult.Buffer);
+        for (int i = 0; i < remaining; i++)
         {
-            *buffer++ = (byte)(i %4 != 2 ? 0xFF:0);
+            if (seq.Remaining < BytesPerPixel)
+            {
+                reader.AdvanceTo(seq.Position, readResult.Buffer.End);
+                return i;
+            }
+
+            seq.TryRead(out *(localPointer+2));
+            seq.TryRead(out *(localPointer+1));
+            seq.TryRead(out *localPointer);
+            localPointer += 3;            
+            *localPointer++ = (byte)0xff;
         }
-        return ValueTask.CompletedTask;
+
+        var TSpan = new Span<byte>(buffer, 4 * totalPixels);
+        return remaining;
     }
 }
