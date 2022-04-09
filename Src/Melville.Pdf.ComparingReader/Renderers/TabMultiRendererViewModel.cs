9e16293b6358c4baeef262c2b8395e20d1e25607
John Melville <johnmelville@gmail.com>
2022-04-09 15:11:49 -0700

    Comparing reader has multiple panes

--- Src/Melville.Pdf.ComparingReader/Renderers/TabMultiRendererViewModel.cs
+++ Src/Melville.Pdf.ComparingReader/Renderers/TabMultiRendererViewModel.cs
@@ -1,19 +1,53 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Melville.Icc.Model.Tags;
+using Melville.INPC;
 using Melville.Pdf.Wpf.Controls;
 
 namespace Melville.Pdf.ComparingReader.Renderers;
 
-public class TabMultiRendererViewModel : MultiRenderer
+public partial class TabMultiRendererViewModel : MultiRenderer
 {
-    public IRenderer First => Renderers[0];
-    public IRenderer Second => Renderers[1];
-    public IRenderer Third => Renderers[2];
-    public IEnumerable<IRenderer> Remaining => Renderers.Skip(3);
+    private readonly IList<IRenderer> renderers;
+
+    public CellConfiguration[] Configurations { get; } =
+    {
+        new(1,1, "Single"),
+        new(2,1, "Horizontal"),
+        new(2,2, "Vertical"),
+        new(4,2, "Quad"),
+        new(6,3, "Six")
+    };
+
+    [AutoNotify] private CellConfiguration currentConfiguration;
+
+    private void OnCurrentConfigurationChanged(CellConfiguration config)
+    {
+        Columns = config.Columns;
+        Panes = Enumerable.Range(0, config.Cells).Select(i => new RenderTab(renderers, i)).ToArray();
+    }
+    [AutoNotify] private IReadOnlyList<RenderTab> panes;
+    [AutoNotify] private int columns = 2;
     public IPageSelector PageSelector { get; }
     public TabMultiRendererViewModel(IList<IRenderer> renderers, IPageSelector pageSelector) : base(
         (IReadOnlyList<IRenderer>)renderers, pageSelector)
     {
+        this.renderers = renderers;
         PageSelector = pageSelector;
+        CurrentConfiguration = Configurations[3];
+    }
+}
+
+public record CellConfiguration(int Cells, int Columns, string Name);
+
+public partial class RenderTab
+{
+    public IList<IRenderer> Renderers { get; }
+    [AutoNotify] private int selected;
+
+    public RenderTab(IList<IRenderer> renderers, int pos)
+    {
+        Renderers = renderers;
+        selected = pos;
     }
 }
