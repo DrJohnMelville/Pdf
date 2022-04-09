504f1501943c17b5b6179a6ad02777492950c5c5
John Melville <johnmelville@gmail.com>
2022-04-09 16:25:29 -0700

    Can go back in the low level viewer

--- Src/Melville.Pdf.LowLevelViewerParts/LowLevelViewer/LowLevelViewModel.cs
+++ Src/Melville.Pdf.LowLevelViewerParts/LowLevelViewer/LowLevelViewModel.cs
@@ -26,13 +26,24 @@ public partial class LowLevelViewModel
 
     public async void SetStream(Stream source)
     {
-        
         Root = await parser.ParseAsync(source, waiter ?? new FakeWaitingService());
     }
 
-    public async Task JumpToReference(ReferencePartViewModel target, IWaitingService waiting)
+    public async ValueTask JumpToReference(ReferencePartViewModel target, IWaitingService waiting)
     {
+        if (targetHistory.Count == 0 || targetHistory.Peek()!= Selected) targetHistory.Push(Selected);
         Selected = (await new DocumentPartSearcher(target.RefersTo, waiting)
             .FindAsync(root)) ?? Selected;
+    }
+
+    private readonly Stack<DocumentPart> targetHistory = new();
+
+    public void NavigateBack()
+    {
+        if (targetHistory.TryPop(out var sel))
+        {
+            sel.Selected = true;
+            Selected = sel;
+        }
     }
 }
