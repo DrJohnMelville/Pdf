807def642dc00fafa7711f1d64cc1f5648abb53b
John Melville <johnmelville@gmail.com>
2022-04-13 12:14:56 -0700

    remove fwont width hack

--- Src/Melville.Pdf.Model/Renderers/FontRenderings/FontWidthParser.cs
+++ Src/Melville.Pdf.Model/Renderers/FontRenderings/FontWidthParser.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Melville.INPC;
+using Melville.Parsing.AwaitConfiguration;
+using Melville.Pdf.LowLevel.Model.Conventions;
 using Melville.Pdf.Model.Documents;
+using Melville.Pdf.Model.Renderers.FontRenderings.Type3;
 
 namespace Melville.Pdf.Model.Renderers.FontRenderings;
 
@@ -8,6 +13,8 @@ public readonly struct FontWidthParser
     private readonly IRealizedFont innerFont;
     private readonly PdfFont pdfFont;
     private readonly double sizeFactor;
+#warning this constant has to go away
+    private const double TerribleBrokenMagicConstantIDoNotUnderstandYet = 17.0/16.0;
 
     public FontWidthParser(IRealizedFont innerFont, PdfFont pdfFont, double size)
     {
@@ -16,5 +23,52 @@ public readonly struct FontWidthParser
         sizeFactor = size /1000;
     }
 
-    public ValueTask<IRealizedFont> Parse(int subTypeKey) => new (innerFont);
+    public ValueTask<IRealizedFont> Parse(int subTypeKey) => subTypeKey switch
+    {
+        KnownNameKeys.Type3 => new(innerFont),
+        KnownNameKeys.Type0 => new(innerFont),
+        _ => ParseSimpleFont()
+    };
+
+    private async ValueTask<IRealizedFont> ParseSimpleFont()
+    {
+        var pdfWidths = await pdfFont.WidthsArrayAsync().CA();
+        return pdfWidths is null ? 
+            innerFont : 
+            new SimpleWidthAdjustedFont(innerFont, await pdfFont.FirstCharAsync().CA(), PreMultiply(pdfWidths));
+    }
+
+    private double[] PreMultiply(double[] pdfWidths)
+    {   
+        for (int i = 0; i < pdfWidths.Length; i++)
+        {
+            pdfWidths[i] *= sizeFactor;
+        }
+        return pdfWidths;
+    }
+}
+
+public partial class SimpleWidthAdjustedFont : IRealizedFont
+{
+
+    [DelegateTo()]
+    private readonly IRealizedFont innerFont;
+    private readonly uint first;
+    private readonly double[] pdfWidths;
+
+    public SimpleWidthAdjustedFont(IRealizedFont innerFont, uint first, double[] pdfWidths)
+    {
+        this.innerFont = innerFont;
+        this.first = first;
+        this.pdfWidths = pdfWidths;
+    }
+    
+    public double AdjustWidth(uint character, double glyphWidth)
+    {
+        var index = character - first;
+        var adjustWidth = index < pdfWidths.Length ? 
+            pdfWidths[index]:
+            innerFont.AdjustWidth(character, glyphWidth);
+        return adjustWidth;
+    }
 }
--- Src/Melville.Pdf.ReferenceDocuments/Text/BuiltInTrueType.cs
+++ Src/Melville.Pdf.ReferenceDocuments/Text/BuiltInTrueType.cs
@@ -13,3 +13,26 @@ public class BuiltInTrueType : FontDefinitionTest
             .WithItem(KnownNames.BaseFont, NameDirectory.Get("CooperBlack"))
             .AsDictionary();
 }
+
+public class ExplicitType1FontWidth: FontDefinitionTest
+{
+    public ExplicitType1FontWidth() : 
+        base("Uses a built in font but specifies font widths")
+    {
+        TextToRender = "ABCABC";
+    }
+
+    protected override PdfObject CreateFont(ILowLevelDocumentCreator arg) =>
+        new DictionaryBuilder()
+            .WithItem(KnownNames.Type, KnownNames.Font)
+            .WithItem(KnownNames.Subtype, KnownNames.Type1)
+            .WithItem(KnownNames.BaseFont, BuiltInFontName.Helvetica)
+            .WithItem(KnownNames.FirstChar, 'A')
+            .WithItem(KnownNames.LastChar, 'C')
+            .WithItem(KnownNames.Widths, new PdfArray(
+                    new PdfInteger(250),
+                    new PdfInteger(1500),
+                    new PdfInteger(1000)
+                ))
+            .AsDictionary();
+}
