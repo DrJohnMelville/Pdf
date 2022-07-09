37b7a8b19e4a7fccde3e1b2f1e7d3bd8883908b9
John Melville <JohnMelville@gmail.com>
2022-07-09 13:44:43 -0400

    Test of type 1 shader pattern using an array of 3 different

--- Src/Melville.Pdf.ReferenceDocuments/Graphics/Patterns/Shading/Type1FunctionalShader.cs
+++ Src/Melville.Pdf.ReferenceDocuments/Graphics/Patterns/Shading/Type1FunctionalShader.cs
@@ -1,49 +1,40 @@
 ﻿using System.Collections.Immutable;
-using System.Numerics;
 using Melville.Pdf.LowLevel.Model.Wrappers.Functions;
 
 namespace Melville.Pdf.ReferenceDocuments.Graphics.Patterns.Shading;
-
-public abstract class Type1FunctionalShaderBase: PatternDisplayClass
-{
-    protected Type1FunctionalShaderBase(string helpText) : base(helpText)
-    {
-    }
-
-    private PdfObject? function = null;
-    protected override async ValueTask SetPagePropertiesAsync(PageCreator page)
-    {
-        var fbuilder = new SampledFunctionBuilder(4, SampledFunctionOrder.Linear);
-        fbuilder.AddInput(2, new ClosedInterval(0, 1));
-        fbuilder.AddInput(2, new ClosedInterval(0,1));
-        fbuilder.AddOutput((x,y)=>x, new ClosedInterval(0,1));
-        fbuilder.AddOutput((x,y)=>y, new ClosedInterval(0,1));
-        fbuilder.AddOutput((x,y)=>1-x, new ClosedInterval(0,1));    
-        function = await fbuilder.CreateSampledFunction();
-
-        await base.SetPagePropertiesAsync(page);
-    }
-
-    protected override PdfObject CreatePattern(ILowLevelDocumentCreator arg)
-    {
-        if (function is not { } localFunc) throw new InvalidOperationException("No func defined");
-        var shading = new DictionaryBuilder()
-            .WithItem(KnownNames.Function, arg.Add(localFunc))
-            .WithItem(KnownNames.ShadingType, 1)
-            .WithItem(KnownNames.ColorSpace, KnownNames.DeviceRGB)
-            .AsDictionary();
-        return new DictionaryBuilder()
-            .WithItem(KnownNames.Shading, arg.Add(shading))
-            .WithItem(KnownNames.Matrix, Matrix3x2.CreateScale(5*72,3*72).AsPdfArray())
-            .WithItem(KnownNames.PatternType, 2)
-            .AsDictionary();
-    }
-}
 
 public class Type1FunctionalShader : Type1FunctionalShaderBase
 {
     public Type1FunctionalShader() : base("Type 1 shader from a pdffunction")
     {
         
+    }
+}
+
+public class Type1TrippleFunctions : Type1FunctionalShaderBase
+{
+    public Type1TrippleFunctions() : base("Type 1 Shader with an array of functions")
+    {
+    }
+    
+    protected override async Task<PdfStream[]> BuildFunction()
+    {
+        var ret = new []
+        {
+            await SingleMethod((x, y) => x),   
+            await SingleMethod((x, y) => y),   
+            await SingleMethod((x, y) => (x+y) / 2.0)   
+        };
+        return ret;
+    }
+
+    private static async Task<PdfStream> SingleMethod(Func<double, double, double> defn)
+    {
+        var fbuilder = new SampledFunctionBuilder(4, SampledFunctionOrder.Linear);
+        fbuilder.AddInput(2, new ClosedInterval(0, 1));
+        fbuilder.AddInput(2, new ClosedInterval(0, 1));
+        fbuilder.AddOutput(defn, new ClosedInterval(0, 1));
+        var ret = await fbuilder.CreateSampledFunction();
+        return ret;
     }
 }
---
+++ Src/Melville.Pdf.ReferenceDocuments/Graphics/Patterns/Shading/Type1FunctionalShaderBase.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+using Melville.Pdf.LowLevel.Model.Wrappers.Functions;
+
+namespace Melville.Pdf.ReferenceDocuments.Graphics.Patterns.Shading;
+
+public abstract class Type1FunctionalShaderBase: PatternDisplayClass
+{
+    protected Type1FunctionalShaderBase(string helpText) : base(helpText)
+    {
+    }
+
+    private PdfObject[]? function = null;
+    protected override async ValueTask SetPagePropertiesAsync(PageCreator page)
+    {
+        function = await BuildFunction();
+        await base.SetPagePropertiesAsync(page);
+    }
+
+    protected virtual async Task<PdfStream[]> BuildFunction()
+    {
+        var fbuilder = new SampledFunctionBuilder(4, SampledFunctionOrder.Linear);
+        fbuilder.AddInput(2, new ClosedInterval(0, 1));
+        fbuilder.AddInput(2, new ClosedInterval(0, 1));
+        fbuilder.AddOutput((x, y) => x, new ClosedInterval(0, 1));
+        fbuilder.AddOutput((x, y) => y, new ClosedInterval(0, 1));
+        fbuilder.AddOutput((x, y) => 1 - x, new ClosedInterval(0, 1));
+        var ret = await fbuilder.CreateSampledFunction();
+        return new PdfStream[]{ret};
+    }
+
+    protected override PdfObject CreatePattern(ILowLevelDocumentCreator arg)
+    {
+        if (function is not { } localFunc) throw new InvalidOperationException("No func defined");
+        var shading = new DictionaryBuilder()
+            .WithItem(KnownNames.Function, arg.Add(ComputeLocalFunc(localFunc, arg)))
+            .WithItem(KnownNames.ShadingType, 1)
+            .WithItem(KnownNames.ColorSpace, KnownNames.DeviceRGB)
+            .AsDictionary();
+        return new DictionaryBuilder()
+            .WithItem(KnownNames.Shading, arg.Add(shading))
+            .WithItem(KnownNames.Matrix, Matrix3x2.CreateScale(5*72,3*72).AsPdfArray())
+            .WithItem(KnownNames.PatternType, 2)
+            .AsDictionary();
+    }
+
+    private static PdfObject ComputeLocalFunc(PdfObject[] localFunc, ILowLevelDocumentCreator ldc)
+    {
+        if (localFunc.Length == 1) return localFunc[0];
+        return new PdfArray(localFunc.Select(i => ldc.Add(i)));
+    }
+}
