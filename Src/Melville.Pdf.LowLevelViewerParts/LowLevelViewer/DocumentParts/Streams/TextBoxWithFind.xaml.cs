e438a8156760feeed320cca3fb78ad0e893efb4a
John Melville <JohnMelville@gmail.com>
2022-07-18 06:40:32 -0400

    Can search within the text box in viewer.

---
+++ Src/Melville.Pdf.LowLevelViewerParts/LowLevelViewer/DocumentParts/Streams/TextBoxWithFind.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using Melville.INPC;
+
+namespace Melville.Pdf.LowLevelViewerParts.LowLevelViewer.DocumentParts.Streams;
+
+[GenerateDP(typeof(bool), "IsReadOnly")]
+[GenerateDP(typeof(FontFamily), "FontFamily")]
+[GenerateDP(typeof(string), "Text")]
+public partial class TextBoxWithFind : UserControl
+{
+    public TextBoxWithFind()
+    {
+        InitializeComponent();
+    }
+
+    private void FindNext(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            DoFindNext();
+        }
+        catch (Exception )
+        {
+        }
+    }
+
+    private void DoFindNext()
+    {
+        var query = new Regex(QueryBox.Text);
+        var hits = query.Matches(DisplayBox.Text);
+        var target = hits
+            .Where(i => i.Index > DisplayBox.SelectionStart)
+            .Concat(hits)
+            .FirstOrDefault();
+        if (target == null) return;
+        DisplayBox.Focus();
+        DisplayBox.Select(target.Index, target.Length);
+        DisplayBox.ScrollToLine(LinesBefore(target.Index));
+        NextButton.Focus();
+    }
+
+    private static readonly Regex LineCounter = new Regex("[\r\n]+");
+    private int LinesBefore(int targetIndex)
+    {
+        return LineCounter.Matches(DisplayBox.Text[..targetIndex]).Count;
+    }
+}
