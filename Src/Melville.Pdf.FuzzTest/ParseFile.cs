d3ff9fb0dba6fc9969b260ce4a265a5d4be6b500
John Melville <JohnMelville@gmail.com>
2022-08-04 17:03:54 -0400

    Fuzzer finds us pages that do not work.

--- Src/Melville.Pdf.FuzzTest/ParseFile.cs
+++ Src/Melville.Pdf.FuzzTest/ParseFile.cs
@@ -1,8 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Melville.Pdf.LowLevel.Parsing.FileParsers;
 using Melville.Pdf.LowLevel.Visitors;
+using Melville.Pdf.Model.Documents;
+using Melville.Pdf.Model.Renderers;
+using Melville.Pdf.Model.Renderers.FontRenderings.DefaultFonts;
+using Melville.Pdf.SkiaSharp;
 
 namespace Melville.Pdf.FuzzTest;
 
@@ -16,16 +21,38 @@ public static class ParseFile
 
     private static async ValueTask Do(FileStream source)
     {
-        var visitor = new TouchEverythingVisitor();
-        var lld = await RandomAccessFileParser.Parse(source);
-        foreach (var item in lld.Objects.Values)
+        try
         {
-            item.Visit(visitor);
+            var doc = await DocumentRendererFactory.CreateRendererAsync(
+                await PdfDocument.ReadAsync(source), WindowsDefaultFonts.Instance);
+            for (int i = 0; i < doc.TotalPages; i++)
+            {
+                Console.Write(".");
+                await RenderPage(doc, i);
+            }
+        }
+        catch (Exception e)
+        {
+            OutputException(-1, e);
         }
     }
-        
-    private class TouchEverythingVisitor: RecursiveDescentVisitor<int>
+
+    private static async Task RenderPage(DocumentRenderer doc, int page)
     {
-            
+        try
+        {
+            await RenderWithSkia.ToSurface(doc, page);
+        }
+        catch (Exception e)
+        {
+            OutputException(page, e);
+        }
+    }
+
+    private static void OutputException(int page, Exception e)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Page {page}");
+        Console.WriteLine(e.Message);
     }
 }
