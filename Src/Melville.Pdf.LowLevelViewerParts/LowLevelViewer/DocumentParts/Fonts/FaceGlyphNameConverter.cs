4b53207641702fbaac9ec76074b267db9314b40d
John Melville <JohnMelville@gmail.com>
2022-08-08 04:07:46 -0400

    Show glyph names in fonts display for the low level viewer.

---
+++ Src/Melville.Pdf.LowLevelViewerParts/LowLevelViewer/DocumentParts/Fonts/FaceGlyphNameConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Data;
+using Melville.MVVM.Wpf.Bindings;
+using Melville.Pdf.Model.Renderers.FontRenderings.FreeType;
+using SharpFont;
+
+namespace Melville.Pdf.LowLevelViewerParts.LowLevelViewer.DocumentParts.Fonts;
+
+public record GlyphNameRecord(uint Glyph, string Name)
+{
+    public override string ToString() => $"0x{Glyph:X} => {Name}  ";
+}
+
+public static class FaceGlyphNameConverter
+{
+    public static readonly IValueConverter Instance = 
+        LambdaConverter.Create<Face,IEnumerable<GlyphNameRecord>>(ExtractGlyphNames);
+
+    private static IEnumerable<GlyphNameRecord> ExtractGlyphNames(Face face) => 
+        face.AllGlyphNames().Select(i => new GlyphNameRecord(i.Glyph, i.Name));
+}
