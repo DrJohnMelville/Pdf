70ee75fee42e6540be64b7920980ea8775f80a96
John Melville <JohnMelville@gmail.com>
2022-08-15 17:48:32 -0400

    Has a test case for CMYK encoded JPEG images that does not work yet.

--- Src/Melville.Pdf.ReferenceDocuments/Graphics/Images/JpegImage.cs
+++ Src/Melville.Pdf.ReferenceDocuments/Graphics/Images/JpegImage.cs
@@ -8,8 +8,7 @@ public class JpegImage: DisplayImageTest
     public JpegImage() : base("Draw a simple, JPEG image")
     {
     }
-
-
+    
     protected override PdfStream CreateImage()
     {
         using var img = GetType().Assembly
@@ -20,6 +19,28 @@ public class JpegImage: DisplayImageTest
             .WithItem(KnownNames.ColorSpace, KnownNames.DeviceRGB)
             .WithItem(KnownNames.Width, new PdfInteger(256))
             .WithItem(KnownNames.Height, new PdfInteger(256))
+            .WithItem(KnownNames.BitsPerComponent, new PdfInteger(8))
+            .WithFilter(FilterName.DCTDecode)
+            .AsStream(img!, StreamFormat.DiskRepresentation);
+    }
+}
+
+public class CmykJpegImage: DisplayImageTest
+{
+    public CmykJpegImage() : base("JPEG image encoded using CMYK")
+    {
+    }
+    
+    protected override PdfStream CreateImage()
+    {
+        using var img = GetType().Assembly
+            .GetManifestResourceStream("Melville.Pdf.ReferenceDocuments.Graphics.Images.CMYKJpeg.jpg");
+        return new DictionaryBuilder()
+            .WithItem(KnownNames.Type, KnownNames.XObject)
+            .WithItem(KnownNames.Subtype, KnownNames.Image)
+            .WithItem(KnownNames.ColorSpace, KnownNames.DeviceCMYK)
+            .WithItem(KnownNames.Width, new PdfInteger(70))
+            .WithItem(KnownNames.Height, new PdfInteger(65))
             .WithItem(KnownNames.BitsPerComponent, new PdfInteger(8))
             .WithFilter(FilterName.DCTDecode)
             .AsStream(img!, StreamFormat.DiskRepresentation);
