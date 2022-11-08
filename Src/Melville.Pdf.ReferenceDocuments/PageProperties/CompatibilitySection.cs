08c8ac242733b6f25813ddba2bf7c66ca2195ce6
John Melville <JohnMelville@gmail.com>
2022-11-08 05:20:12 -0500

    Add test for compatibility sections

---
+++ Src/Melville.Pdf.ReferenceDocuments/PageProperties/CompatibilitySection.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using Melville.Pdf.LowLevel.Writers.ContentStreams;
+using Melville.Pdf.ReferenceDocuments.Graphics;
+
+namespace Melville.Pdf.ReferenceDocuments.PageProperties;
+
+public class CompatibilitySection : Card3x5
+{
+    public CompatibilitySection() : base("Ignore unrecognized operators between BX and EX operators")
+    {
+    }
+
+    protected override void DoPainting(ContentStreamWriter csw)
+    {
+        csw.SetLineWidth(10);
+        csw.MoveTo(10,10);
+        csw.LineTo(150,10);
+        csw.StrokePath();
+        csw.ModifyTransformMatrix(Matrix3x2.CreateTranslation(100, 100));
+        using var _ = csw.BeginCompatibilitySection();
+        csw.SetLineWidth(10);
+        csw.MoveTo(10,10);
+        csw.WriteLiteral("(This operator doesn't exist.) JDM\r\n");
+        csw.LineTo(150,10);
+        csw.StrokePath();
+    }
+}
