7aaf689a223cb53ff278d67a2e9f0102725440fa
John Melville <JohnMelville@gmail.com>
2022-12-19 13:37:43 -0500

    Single public Skia renderer class has doc comments.

--- Src/Melville.Pdf.SkiaSharp/RenderWithSkia.cs
+++ Src/Melville.Pdf.SkiaSharp/RenderWithSkia.cs
@@ -6,17 +6,23 @@ using SkiaSharp;
 
 namespace Melville.Pdf.SkiaSharp
 {
+    /// <summary>
+    /// This is a facade class that is used to render pdf document pages using Google's skia drawing library.
+    /// </summary>
     public static class RenderWithSkia
     {
         /// <summary>
         /// Render a given page of a pdf document to a PNG formatted image
+        ///
+        /// Height and width are optional parameters.  If only one is specified the other will be computed
+        /// to maintain the aspect ratio of the PDF viewport.  If neither is specified, the image will be
+        /// the size of the PDF viewport.
         /// </summary>
         /// <param name="doc">The PDF document to render</param>
         /// <param name="oneBasedPageNumber">The page number to render, first page being page 1.</param>
         /// <param name="target">A writable stream to receive the output PNG bits.</param>
         /// <param name="width">Optional parameter for the width of the desired image.</param>
-        /// <param name="height">Optional par</param>
-        /// <returns></returns>
+        /// <param name="height">Optional parameter for the height of the desired image.</param>
         public static async ValueTask ToPngStreamAsync(
             DocumentRenderer doc, int oneBasedPageNumber, Stream target, int width = -1, int height = -1)
         {
@@ -26,6 +32,18 @@ namespace Melville.Pdf.SkiaSharp
             data.SaveTo(target);
         }
 
+        /// <summary>
+        /// Render a given page of a pdf document to a Skia SKSurface.
+        ///
+        /// Height and width are optional parameters.  If only one is specified the other will be computed
+        /// to maintain the aspect ratio of the PDF viewport.  If neither is specified, the image will be
+        /// the size of the PDF viewport.
+        /// </summary>
+        /// <param name="doc">The PDF document to render</param>
+        /// <param name="oneBasedPageNumber">The page number to render, first page being page 1.</param>
+        /// <param name="width">Optional parameter for the width of the desired image.</param>
+        /// <param name="height">Optional parameter for the height of the desired image.</param>
+        /// <returns>A SKSurface object with the selected page rendered upon it.</returns>
         public static async ValueTask<SKSurface> ToSurfaceAsync(
             DocumentRenderer doc, int oneBasedPageNumber, int width = -1, int height = -1)
         {
