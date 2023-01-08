88ef905624fbbfb4ea7ed506130fdba99472c632
John Melville <JohnMelville@gmail.com>
2023-01-07 20:02:44 -0500

    Make my own copy of SharpFont

---
+++ Src/Melville.SharpFont/ArchitectureDllImport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace SharpFont.NetStandard
+{
+	public static class ArchitectureDllImport
+	{
+		public const string DefaultDllDirectory = "";
+		public static Dictionary<Architecture, string> ArchitectureDlls = new Dictionary<Architecture, string>()
+		{
+			{ Architecture.X64, "x64" },
+			{ Architecture.X86, "x86" },
+			{ Architecture.Arm, DefaultDllDirectory },
+			{ Architecture.Arm64, DefaultDllDirectory }
+		};
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool SetDllDirectory(string lpPathName);
+
+		public static void LoadArchitectureDependencyDirectory()
+		{
+			if (!ArchitectureDlls.TryGetValue(RuntimeInformation.ProcessArchitecture, out var dllPath))
+			{
+				dllPath = DefaultDllDirectory;
+			}
+
+			if (!string.IsNullOrWhiteSpace(dllPath))
+			{
+				var path = Path.Combine(Directory.GetCurrentDirectory(), "lib", dllPath);
+				if (Directory.Exists(path))
+				{
+					SetDllDirectory(path);
+				}
+			}
+		}
+	}
+}
---
+++ Src/Melville.SharpFont/MarshalNotNull.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFont;
+
+internal class MarshalNotNull
+{
+    public static string PtrToStringAnsi(nint ptr) => Marshal.PtrToStringAnsi(ptr)??"";
+    public static string PtrToStringAnsi(nint ptr, int len) => Marshal.PtrToStringAnsi(ptr, len)??"";
+}
