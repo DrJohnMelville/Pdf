a751d17ef9155e60b9de53c1c97a4ee041660ed1
John Melville <JohnMelville@gmail.com>
2024-05-03 16:24:40 -0400

    updates

---
+++ Src/TimeTrial/Minimal.cs
@@ -0,0 +1,23 @@
+﻿using Melville.Pdf.LowLevel.Model.Document;
+using Melville.Pdf.LowLevel.Writers.DocumentWriters;
+using Melville.Pdf.Model.Creators;
+using Melville.Pdf.ReferenceDocuments.Infrastructure;
+
+namespace Melville.Pdf.ReferenceDocuments.LowLevel;
+
+public class MinimalPdfParser: CreatePdfParser
+{
+    public MinimalPdfParser() : base("Minimal pdf with a single blank page.")
+    {
+    }
+
+    public override async ValueTask WritePdfAsync(Stream target) =>
+        await MinimalPdf().WriteToAsync(target);
+
+    public static PdfLowLevelDocument MinimalPdf(byte major = 1, byte minor = 7)
+    {
+        var builder = new PdfDocumentCreator();
+        builder.Pages.CreatePage();
+        return builder.CreateDocument(major, minor);
+    }
+}
