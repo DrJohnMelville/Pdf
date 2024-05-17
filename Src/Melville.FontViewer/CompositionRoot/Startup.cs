ff2f7afeceadd1cf3ace16709cc1fe631c6b9452
John Melville <JohnMelville@gmail.com>
2024-05-17 01:59:34 -0400

    Font reader and viewer projects created

---
+++ Src/Melville.FontViewer/CompositionRoot/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using Melville.FontViewer.Home;
+using Melville.IOC.IocContainers;
+using Melville.MVVM.Wpf.MvvmDialogs;
+using Melville.MVVM.Wpf.RootWindows;
+using Melville.Pdf.LowLevel.Parsing.ParserContext;
+using Melville.Pdf.LowLevelViewerParts.ExceptionDumpers;
+using Melville.Pdf.LowLevelViewerParts.FakeFileDialog;
+using Melville.WpfAppFramework.StartupBases;
+
+namespace Melville.FontViewer.CompositionRoot;
+
+public class Startup:StartupBase
+{
+    [STAThread]
+    public static void Main(string[] arguments)
+    {
+        ApplicationRootImplementation.Run(new Startup(arguments));
+    }
+
+    private Startup(string[] arguments) : base(arguments)
+    {
+        ExceptionDumper.RegisterExceptionDumper();
+    }
+
+    protected override void RegisterWithIocContainer(IBindableIocService service)
+    {
+        service.RegisterHomeViewModel<HomeViewModel>();
+        service.Bind<IOpenSaveFile>().To<OpenSaveFileAdapter>();
+        service.Bind<Window>().And<IRootNavigationWindow>().To<RootNavigationWindow>().AsSingleton();
+        TryRegistedCommandLine(service);
+    }
+    
+    private void TryRegistedCommandLine(IBindableIocService service)
+    {
+        if (CommandLineParameters.Length > 0 && File.Exists(CommandLineParameters[0]))
+        {
+            service.Bind<IOpenSaveFile>().ToConstant(
+                new FakeOpenAdapter(CommandLineParameters[0]));
+        }
+    }
+
+}
---
+++ Src/Melville.FontViewer/Home/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Melville.INPC;
+using Melville.MVVM.Wpf.DiParameterSources;
+using Melville.MVVM.Wpf.MvvmDialogs;
+using Melville.MVVM.Wpf.ViewFrames;
+using Melville.SharpFont;
+using System.IO;
+using System.Windows;
+
+namespace Melville.FontViewer.Home;
+
+[OnDisplayed(nameof(LoadFile))]
+public partial class HomeViewModel
+{
+    [AutoNotify] private Stream? data = null;
+
+    public async void LoadFile([FromServices] IOpenSaveFile osf)
+    {
+        var file = osf.GetLoadFile(null, "ttf", 
+            "All Font Files (ttf;ttc;fon)|*.ttf;*.ttc;*.fon|" +
+            "True Type Font Files (*.ttf)|*.ttf|" +
+            "True Type Collections (*.ttc)|*.ttc|" +
+            "Font files (*.fon)|*.fon", "Select File to View");
+        if (file == null) 
+            Application.Current.Shutdown();
+        else
+            Data = await file.OpenRead();
+    }
+
+}
