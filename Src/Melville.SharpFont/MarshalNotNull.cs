27c9fb6ee54f6953de8cf5bb2ea2c0d375bc73f2
John Melville <JohnMelville@gmail.com>
2024-09-04 16:46:06 -0400

    Remove  SharpFont because we don't need it anymore.

--- Src/Melville.SharpFont/MarshalNotNull.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace Melville.SharpFont;
-
-internal class MarshalNotNull
-{
-    public static string PtrToStringAnsi(nint ptr) => Marshal.PtrToStringAnsi(ptr)??"";
-    public static string PtrToStringAnsi(nint ptr, int len) => Marshal.PtrToStringAnsi(ptr, len)??"";
-}
