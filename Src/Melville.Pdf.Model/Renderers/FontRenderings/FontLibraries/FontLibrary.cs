6f10fcf777512528252bd7f94b9f0c7ab5ae3a6b
John Melville <JohnMelville@gmail.com>
2025-07-01 17:35:07 -0400

    Fix system fonts name font -Bold -Italic or -Oblique

--- Src/Melville.Pdf.Model/Renderers/FontRenderings/FontLibraries/FontLibrary.cs
+++ Src/Melville.Pdf.Model/Renderers/FontRenderings/FontLibraries/FontLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using Melville.Pdf.LowLevel.Model.Conventions;
 
 namespace Melville.Pdf.Model.Renderers.FontRenderings.FontLibraries;
@@ -14,12 +15,25 @@ internal class FontLibrary
 
     public FontReference? FontFromName(in ReadOnlySpan<byte> fontName, bool bold, bool italic)
     {
-//        var strName = fontName.ExtendedAsciiString();
-        return FindFontFamily(fontName)?.SelectFace(bold, italic);
+        return 
+            TrySuffix(fontName, "-Bold"u8, true, italic) ??
+            TrySuffix(fontName, "-BoldItalic"u8, true, true) ??
+            TrySuffix(fontName, "-Italic"u8, bold, true) ??
+            TrySuffix(fontName, "-BoldOblique"u8, true, true) ??
+            TrySuffix(fontName, "-Oblique"u8, bold, true) ??
+            FindFontFamily(fontName)?.SelectFace(bold, italic);
+    }
+
+    public FontReference? TrySuffix(ReadOnlySpan<byte> fontName, ReadOnlySpan<byte> suffix, 
+        bool bold, bool italic)
+    {
+        if (!fontName.EndsWith(suffix)) return null;
+        return FontFromName(fontName[..^suffix.Length], bold, italic);
     }
 
     private FontFamily? FindFontFamily(in ReadOnlySpan<byte> fontName)
     {
+        var familyNameString = Encoding.UTF8.GetString(fontName);
         FontFamily? ret = null;
         int currentLen = int.MaxValue;
         foreach (var family in families)
